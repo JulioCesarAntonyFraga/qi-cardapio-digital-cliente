1d420d250bf140809ea17b4abc12e5cf8e5229f4
si-jufr <jufr@siteimprove.com>
2024-12-02 12:56:25 +0000

    fix addProduct extra ;

--- scripts/AddToCart.js
+++ scripts/AddToCart.js
@@ -2,7 +2,7 @@ var maxAmount = 10;
 
 function addProduct(element){
     var actionsElement = element.parentNode;
-    var amountElement = actionsElement.querySelector("#amount");;
+    var amountElement = actionsElement.querySelector("#amount");
     var amount = Number(amountElement.innerHTML);
 
     if(amount >= maxAmount){
